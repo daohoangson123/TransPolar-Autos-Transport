a1042fd1e761378cac0f9d40eb68352930220f9f
daohoangson123 <daohoangson2109>
2022-09-25 10:23:20 +0700

    123

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-
-
-//
-
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -44,59 +40,59 @@ window.onclick = function(event) {
 
 /* click carousel*/
 
-// let slideIndex = 1;
-// showSlides(slideIndex);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-// function plusSlides(n) {
-//     showSlides((slideIndex += n));
-// }
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
 
-// function currentSlide(n) {
-//     showSlides((slideIndex = n));
-// }
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
 
-// function showSlides(n) {
-//     let i;
-//     let slides = document.getElementsByClassName("mySlides");
-//     let dots = document.getElementsByClassName("dot");
-//     if (n > slides.length) {
-//     slideIndex = 1;
-//     }
-//     if (n < 1) {
-//     slideIndex = slides.length;
-//     }
-//     for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].style.display = "block";
-//     dots[slideIndex - 1].className += " active";
-// }
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+    slideIndex = 1;
+    }
+    if (n < 1) {
+    slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
 
 //
 
 /* auto carousel */
 
-{
-    let slideIndex = 0;
-showSlides();
+// {
+//     let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every # seconds
-    }
-}
+// function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("mySlides");
+//     let dots = document.getElementsByClassName("dot");
+//     for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}    
+//     for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";  
+//     dots[slideIndex-1].className += " active";
+//     setTimeout(showSlides, 5000); // Change image every # seconds
+//     }
+// }
