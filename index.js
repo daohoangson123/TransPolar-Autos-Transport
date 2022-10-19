593d1da725f1c6d3937be63d91f53941bd458329
daohoangson123 <daohoangson2109>
2022-10-20 01:04:52 +0700

    123

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// // window.onscroll = function () {
+// //   scrollDown();
+// //   counterNum();
+// // }
+
 // Sticky Nav
 var navbar = document.querySelector('nav')
 
@@ -9,125 +14,138 @@ window.onscroll = function () {
   }
 }
 
-// window.onscroll = function () {
-//   scrollDown();
-//   counterNum();
-// }
-
-// function scrollDown() {
-//   if (window.pageYOffset > 0) {
-//     navbar.classList.add('scrolled')
-//   } else {
-//     navbar.classList.remove('scrolled')
-//   }
-// }
-
-// // Counter
-// var counterUp = document.querySelectorAll('.about-sect2-counter')[3];
-
-// function counterNum() {
-//   if (window.pageYOffset > 500) {
-//     counterUp.classList.add('counter-4')
-//   } else {
-//     counterUp.classList.remove('counter-4')
-//   }
-// }
-
-// Dropdown Btn on Full
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function scrollDown() {
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
   }
+}
+
+// // // Counter
+// // var counterUp = document.querySelectorAll('.about-sect2-counter')[3];
+
+// // function counterNum() {
+// //   if (window.pageYOffset > 500) {
+// //     counterUp.classList.add('counter-4')
+// //   } else {
+// //     counterUp.classList.remove('counter-4')
+// //   }
+// // }
+
+// // Dropdown Btn on Full
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+
+// // Dropdown Btn Hidden
+// function mineFunction() {
+//   document.getElementById("mineDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+//Click carousel
+
+let slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// select next slide button
+let nextSlide = document.querySelector(".next");
+
+let dots = document.getElementsByClassName("dot");
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" active", "");
+}
+
+// add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+
+  // check if current slide is the last and reset current slide
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
   }
 
-// Dropdown Btn Hidden
-function mineFunction() {
-  document.getElementById("mineDropdown").classList.toggle("show");
-}
+  //   move slide by -100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+// select next slide button
+let prevSlide = document.querySelector(".prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
   }
-}
 
-/* click carousel*/
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-    showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-    slideIndex = 1;
-    }
-    if (n < 1) {
-    slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+  //   move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
 
 //
 
-/* auto carousel */
-
-// {
-//     let slideIndex = 0;
+// let slideIndex = 0;
 // showSlides();
 
 // function showSlides() {
-//     let i;
-//     let slides = document.getElementsByClassName("mySlides");
-//     let dots = document.getElementsByClassName("dot");
-//     for (i = 0; i < slides.length; i++) {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   let dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
 //     slides[i].style.display = "none";  
-//     }
-//     slideIndex++;
-//     if (slideIndex > slides.length) {slideIndex = 1}    
-//     for (i = 0; i < dots.length; i++) {
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
 //     dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex-1].style.display = "block";  
-//     dots[slideIndex-1].className += " active";
-//     setTimeout(showSlides, 5000); // Change image every # seconds
-//     }
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
 // }
-
-//
