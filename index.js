0a4d92b2000076db8890ac8c19847d9042ea2030
daohoangson123 <daohoangson2109@gmail.com>
2022-11-08 17:18:47 +0700

    alo

--- index.js
+++ index.js
@@ -5,73 +5,52 @@ window.onscroll = function () {
 }
 
 // Sticky Nav
-var navBar = document.querySelector("nav")
-function stickyNav() {
-  if (window.pageYOffset > 0) {
-    navBar.classList.add("stickyNav")
+$(window).scroll(function(){
+  if (window.pageYOffset > 0){
+    $("nav").addClass("stickyNav");
   } else {
-    navBar.classList.remove("stickyNav")
+    $("nav").removeClass("stickyNav");
   }
-}
+});
+
+// var navBar = document.querySelector("nav")
+// function stickyNav() {
+//   if (window.pageYOffset > 0) {
+//     navBar.classList.add("stickyNav")
+//   } else {
+//     navBar.classList.remove("stickyNav")
+//   }
+// }
 
 // Back to top
-var backTop = document.querySelector(".top")
-function backTopBtn() {
-  if (window.pageYOffset > 500) {
-    backTop.classList.add("backTopBtn")
+$(window).scroll(function(){
+  if (window.pageYOffset > 500){
+    $(".top").addClass("backTopBtn");
   } else {
-    backTop.classList.remove("backTopBtn")
+    $(".top").removeClass("backTopBtn");
   }
-}
+});
 
-// // // Counter
-// // var counterUp = document.querySelectorAll(".about-sect2-counter")[3];
+// var backTop = document.querySelector(".top")
+// function backTopBtn() {
+//   if (window.pageYOffset > 500) {
+//     backTop.classList.add("backTopBtn")
+//   } else {
+//     backTop.classList.remove("backTopBtn")
+//   }
+// }
 
-// // function counterNum() {
-// //   if (window.pageYOffset > 500) {
-// //     counterUp.classList.add("counter-4")
-// //   } else {
-// //     counterUp.classList.remove("counter-4")
-// //   }
-// // }
+// Dropdown Btn Main
 
-// // Dropdown Btn on Full
-function myFunction() {
-    document.querySelector("#myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches(".dropbtn")) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains("show")) {
-          openDropdown.classList.remove("show");
-        }
-      }
-    }
-  }
+$("#dropbtn-main").click(function(){
+  $(".dropdown-content").slideToggle();
+});
 
-// // Dropdown Btn Hidden
+// Dropdown Btn Hidden
 
-function mineFunction() {
-  document.getElementById("mineDropdown").classList.toggle("show");
-}
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
-  }
-}
+$("#dropbtn-hidden").click(function(){
+  $(".drop-content-hidden").slideToggle();
+});
 
 //
 
@@ -128,22 +107,3 @@ prevSlide.addEventListener("click", function () {
 
 // Auto Slide
 
-// let slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     // slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   // slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
