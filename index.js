0188ed3fd538416b20e7b2999607217bcf51fbf1
daohoangson123 <daohoangson2109@gmail.com>
2022-11-10 15:05:12 +0700

    alo

--- index.js
+++ index.js
@@ -1,70 +1,20 @@
-// khai báo nhiều hàm sử dụng cùng chức năng phải khai báo tổng
-// window.onscroll = function () { 
-//   stickyNav();
-//   backTopBtn();
-// }
-
-$(document).ready(function(){
-  // dùng jquery có thể mix với js
-
-  //thêm/xóa class cho nav khi scroll
-  $(window).scroll(function(){
-    if (window.pageYOffset > 0){
-      $("nav").addClass("stickyNav");
-    } else {
-      $("nav").removeClass("stickyNav");
-    }
-  });
-
-  //thêm/xóa class cho nút backtotop
-  $(window).scroll(function(){
-    if (window.pageYOffset > 500){
-      $(".top").addClass("backTopBtn");
-    } else {
-      $(".top").removeClass("backTopBtn");
-    }
-  });
-});
-
+window.onscroll = function() {
 // Sticky Nav
-
-
-// var navBar = document.querySelector("nav")
-// function stickyNav() {
-//   if (window.pageYOffset > 0) {
-//     navBar.classList.add("stickyNav")
-//   } else {
-//     navBar.classList.remove("stickyNav")
-//   }
-// }
-
-// Back to top
-
-// var backTop = document.querySelector(".top")
-// function backTopBtn() {
-//   if (window.pageYOffset > 500) {
-//     backTop.classList.add("backTopBtn")
-//   } else {
-//     backTop.classList.remove("backTopBtn")
-//   }
-// }
-
-// Dropdown Btn Main
-
-$("#dropbtn-main").click(function(){
-  $(".dropdown-content-main").slideToggle();
-});
-
-// Dropdown Btn Hidden
-
-$("#dropbtn-hidden").click(function(){
-  $("#dropin").toggleClass();
-  $("#dropout").toggleClass("show");
-  $(".drop-content-hidden").slideToggle();
-});
-
-
-//
+  let navBar = document.getElementById("navbar");
+  if (window.pageYOffset > 0) {
+    navBar.classList.add("stickyNav");
+  } else {
+    navBar.classList.remove("stickyNav");
+  }
+  
+// Backtotop
+  let backTop = document.getElementsByClassName("top")[0];
+  if (window.pageYOffset > 500) {
+    backTop.classList.add("backTopBtn");
+  } else {
+    backTop.classList.remove("backTopBtn");
+  }
+}
 
 // Click Slide
 
@@ -84,7 +34,7 @@ let maxSlide = slides.length - 1;
 let nextSlide = document.querySelector(".next");
 
 // add event listener and navigation functionality
-nextSlide.addEventListener("click", function () {
+nextSlide.onclick = function () {
 
   // check if current slide is the last and reset current slide
   if (curSlide === maxSlide) {
@@ -97,13 +47,13 @@ nextSlide.addEventListener("click", function () {
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
-});
+};
 
 // select next slide button
 let prevSlide = document.querySelector(".prev");
 
 // add event listener and navigation functionality
-prevSlide.addEventListener("click", function () {
+prevSlide.onclick = function () {
   // check if current slide is the first and reset current slide to last
   if (curSlide === 0) {
     curSlide = maxSlide;
@@ -115,7 +65,45 @@ prevSlide.addEventListener("click", function () {
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
-});
+};
 
 // Auto Slide
 
+//
+
+// dùng jquery có thể mix với js
+$(document).ready(function(){
+
+  //thêm/xóa class cho nav khi scroll
+  // $(window).scroll(function(){
+  //   if (window.pageYOffset > 0){
+  //     $("nav").addClass("stickyNav");
+  //   } else {
+  //     $("nav").removeClass("stickyNav");
+  //   }
+  // });
+
+  //thêm/xóa class cho nút backtotop
+  // $(window).scroll(function(){
+  //   if (window.pageYOffset > 500){
+  //     $(".top").addClass("backTopBtn");
+  //   } else {
+  //     $(".top").removeClass("backTopBtn");
+  //   }
+  // });
+
+  // Dropdown Btn Main
+
+  $("#dropbtn-main").click(function(){
+    $(".dropdown-content-main").slideToggle();
+  });
+
+  // Dropdown Btn Hidden
+
+  $("#dropbtn-hidden").click(function(){
+    $("#dropin").toggleClass();
+    $("#dropout").toggleClass("show");
+    $(".drop-content-hidden").slideToggle();
+  });
+//
+});
