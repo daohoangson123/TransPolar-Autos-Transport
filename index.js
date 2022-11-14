fb29b8d5e7c306e22f55ca0661202ca6da5667eb
daohoangson123 <daohoangson2109@gmail.com>
2022-11-14 13:08:06 +0700

    14/11

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+// ===JS===
 window.onscroll = function () {
-// Sticky Nav
+// StickyNav
   let navBar = document.getElementById("navbar");
-  if (window.pageYOffset > 0) {
+  if (window.pageYOffset > 0 && (!navBar.classList.contains("dark-theme"))) {
     navBar.classList.add("stickyNav");
   } else {
     navBar.classList.remove("stickyNav");
   }
+  
   
 // Backtotop
   let backTop = document.getElementsByClassName("top")[0];
@@ -16,170 +18,11 @@ window.onscroll = function () {
   }
 }
 
-// Click Slide
-
-let slides = document.querySelectorAll(".slide");
-
-// loop through slides and set each slides translateX
-slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * 100}%)`;
-})
-
-// current slide counter
-let curSlide = 0;
-// maximum number of slides
-let maxSlide = slides.length - 1;
-
-// select next slide button
-let nextSlide = document.querySelector(".next");
-
-// add event listener and navigation functionality
-nextSlide.onclick = function () {
-
-  // check if current slide is the last and reset current slide
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
-
-  //   move slide by -100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-};
-
-// select next slide button
-let prevSlide = document.querySelector(".prev");
-
-// add event listener and navigation functionality
-prevSlide.onclick = function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
-
-  //   move slide by 100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-};
-
-//
-let qouteblock = document.querySelectorAll(".customer__content");
-
-// loop through slides and set each slides translateX
-qouteblock.forEach((qoute, indx) => {
-  qoute.style.transform = `translateX(${indx * 100}%)`;
-})
-
-// current slide counter
-let curQoute = 0;
-// maximum number of slides
-let maxQoute = qouteblock.length - 1;
-
-// select next slide button
-let nextQoute = document.querySelector(".ctm__dot--2");
-
-// add event listener and navigation functionality
-nextQoute.onclick = function () {
-
-  // check if current slide is the last and reset current slide
-  if (curQoute === maxQoute) {
-    curQoute = 0;
-  } else {
-    curQoute++;
-  }
-
-  //   move slide by -100%
-  qouteblock.forEach((qoute, indx) => {
-    qoute.style.transform = `translateX(${100 * (indx - curQoute)}%)`;
-  });
-};
-
-// select next slide button
-let prevQoute = document.querySelector(".ctm__dot--1");
-
-// add event listener and navigation functionality
-prevQoute.onclick = function () {
-  // check if current slide is the first and reset current slide to last
-  if (curQoute === 0) {
-    curQoute = maxQoute;
-  } else {
-    curQoute--;
-  }
-
-  //   move slide by 100%
-  qouteblock.forEach((qoute, indx) => {
-    qoute.style.transform = `translateX(${100 * (indx - curQoute)}%)`;
-  });
-};
-
-// Auto Slide
-
-//
-
-// dùng jquery có thể mix với js
-$(document).ready(function(){
-
-  //thêm/xóa class cho nav khi scroll
-  // $(window).scroll(function(){
-  //   if (window.pageYOffset > 0){
-  //     $("nav").addClass("stickyNav");
-  //   } else {
-  //     $("nav").removeClass("stickyNav");
-  //   }
-  // });
-
-  //thêm/xóa class cho nút backtotop
-  // $(window).scroll(function(){
-  //   if (window.pageYOffset > 500){
-  //     $(".top").addClass("backTopBtn");
-  //   } else {
-  //     $(".top").removeClass("backTopBtn");
-  //   }
-  // });
-
-  // Dropdown Btn Main
-  //JQ
-  $("#dropbtn-main").click(function(){
-    $(".dropdown-content-main").slideToggle();
-  });
-  //JS
-
-  // Dropdown Btn Hidden
-  //JQ
-  $("#dropbtn-hidden").click(function(){
-    $("#dropin").toggleClass();
-    $("#dropout").toggleClass("show");
-    $(".drop-content-hidden").slideToggle();
-  });
-  //JS
+//Drop Menu Main/ Hidden
 
 
-  //mở/đóng search dialog = JQ =
-  // $(".search-btn").click(function(){
-  //   $("#search-dialog").slideToggle();
-  // });
 
-  //đóng search dialog bằng nút trong dialog = JQ =
-  // $("#dialog-closebtn").click(function(){
-  //   $("#search-dialog").slideUp();
-  // });
-
-  //mở copllapse content
-  //JQ
-  $(".copllapse__btn").click(function(){
-    $(this).toggleClass("copllapse__btn--gb");
-    $(this).children(".copllapse__down").toggleClass("hide");
-    $(this).children(".copllapse__up").toggleClass("show");
-    $(this).next().slideToggle();
-  });
-});
-
-//mở search dialog = JS =
+//Open Search dialog
 
 let searchBtn = document.getElementsByClassName("search-btn");
 //dùng loop để lấy tất cả các search-btn
@@ -213,3 +56,174 @@ closeDialog.onclick = function () {
   }
   setTimeout(resetTransform , 1000)
 }
+
+//Copllapsible content
+
+// Click Slide
+
+let slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+})
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// select next slide button
+let nextSlide = document.querySelector(".next");
+
+// add event listener and navigation functionality
+nextSlide.onclick = function () {
+
+  // check if current slide is the last and reset current slide
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  // move slide by -100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+};
+
+// select next slide button
+let prevSlide = document.querySelector(".prev");
+
+// add event listener and navigation functionality
+prevSlide.onclick = function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  // move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+};
+
+// Click Testimonial
+let qouteblock = document.querySelectorAll(".customer__content");
+
+// loop through slides and set each slides translateX
+qouteblock.forEach((qoute, indx) => {
+  qoute.style.transform = `translateX(${indx * 100}%)`;
+})
+
+// current slide counter
+let curQoute = 0;
+// maximum number of slides
+let maxQoute = qouteblock.length - 1;
+
+// select next slide button
+let nextQoute = document.querySelector(".ctm__dot--2");
+
+// add event listener and navigation functionality
+nextQoute.onclick = function () {
+
+  // check if current slide is the last and reset current slide
+  if (curQoute === maxQoute) {
+    curQoute = 0;
+  } else {
+    curQoute++;
+  }
+
+  // move slide by -100%
+  qouteblock.forEach((qoute, indx) => {
+    qoute.style.transform = `translateX(${100 * (indx - curQoute)}%)`;
+  });
+};
+
+// select next slide button
+let prevQoute = document.querySelector(".ctm__dot--1");
+
+// add event listener and navigation functionality
+prevQoute.onclick = function () {
+  // check if current slide is the first and reset current slide to last
+  if (curQoute === 0) {
+    curQoute = maxQoute;
+  } else {
+    curQoute--;
+  }
+
+  // move slide by 100%
+  qouteblock.forEach((qoute, indx) => {
+    qoute.style.transform = `translateX(${100 * (indx - curQoute)}%)`;
+  });
+};
+
+// Auto Slide
+
+//
+
+// ===JQ=== jquery có thể mix với js
+$(document).ready(function(){
+
+  //StickyNavbar
+  // $(window).scroll(function(){
+  //   if (window.pageYOffset > 0){
+  //     $("nav").addClass("stickyNav");
+  //   } else {
+  //     $("nav").removeClass("stickyNav");
+  //   }
+  // });
+
+  // backtotop
+  // $(window).scroll(function(){
+  //   if (window.pageYOffset > 500){
+  //     $(".top").addClass("backTopBtn");
+  //   } else {
+  //     $(".top").removeClass("backTopBtn");
+  //   }
+  // });
+
+  // Dropdown Menu Main = JQ =
+  $("#dropbtn-main").click(function(){
+    $(".dropdown-content-main").slideToggle();
+  });
+
+  // Dropdown Menu Hidden = JQ =
+  $("#dropbtn-hidden").click(function(){
+    $("#dropin").toggleClass();
+    $("#dropout").toggleClass("show");
+    $(".drop-content-hidden").slideToggle();
+  });
+
+  //mở/đóng search dialog = JQ =
+  // $(".search-btn").click(function(){
+  //   $("#search-dialog").slideToggle();
+  // });
+
+  //đóng search dialog bằng nút trong dialog = JQ =
+  // $("#dialog-closebtn").click(function(){
+  //   $("#search-dialog").slideUp();
+  // });
+
+  //Dark/Light Btn
+  $(".dark-btn").click(function(){
+    $(".fa-moon").toggleClass("hide");
+    $(".fa-sun").toggleClass("show");
+    $("body, #navbar, .about__mid, #services, .ser-layout2__items .ser-l1-items__content a, .ser-layout2__body--layout, .services-button, .article, .abt-l2-sub__content--sect1 .abt-l2-sub__text a, .abt-sect4__text a, .about__sect5, .services__sect2").toggleClass("dark-theme");
+    if ($("#navbar").hasClass("dark-theme")){
+      $("#navbar").removeClass("stickyNav");
+    } else if (window.pageYOffset > 0) {
+      $("#navbar").addClass("stickyNav");
+    }
+  });
+
+  //Copllapsible content
+  $(".copllapse__btn").click(function(){
+    $(this).toggleClass("copllapse__btn--gb");
+    $(this).children(".copllapse__down").toggleClass("hide");
+    $(this).children(".copllapse__up").toggleClass("show");
+    $(this).next().slideToggle();
+  });
+});
