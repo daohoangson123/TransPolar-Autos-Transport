7f902d5210ed0893aee683777125635bac54b34c
daohoangson123 <108396570+daohoangson123@users.noreply.github.com>
2024-07-29 21:08:21 +0700

    revamp some css

--- index.js
+++ index.js
@@ -1,116 +1,113 @@
 // ===JS===
 window.onscroll = function () {
-// StickyNav
-  let navBar = document.getElementById("navbar");
-  if (window.pageYOffset > 0 && (!navBar.classList.contains("dark-theme"))) { //kiểm tra navbar có đang ở darkmode ko
-    navBar.classList.add("stickyNav");
-  } else {
-    navBar.classList.remove("stickyNav");
-  }
+    // StickyNav
+    let navBar = document.getElementById('navbar');
+    if (window.pageYOffset > 0 && !navBar.classList.contains('dark-theme')) {
+        //kiểm tra navbar có đang ở darkmode ko
+        navBar.classList.add('stickyNav');
+    } else {
+        navBar.classList.remove('stickyNav');
+    }
 
-// Backtotop
-  let backTop = document.getElementsByClassName("top")[0];
-  if (window.pageYOffset > 500) {
-    backTop.classList.add("backTopBtn");
-  } else {
-    backTop.classList.remove("backTopBtn");
-  }
-}
+    // Backtotop
+    let backTop = document.getElementsByClassName('top')[0];
+    if (window.pageYOffset > 500) {
+        backTop.classList.add('backTopBtn');
+    } else {
+        backTop.classList.remove('backTopBtn');
+    }
+};
 
 // Drop Menu Main/ Hidden
 
-
-
 // Search dialog
-let searchDialog = document.getElementById("search-dialog");
+let searchDialog = document.getElementById('search-dialog');
 
-let searchBtn = document.getElementsByClassName("search-btn");
+let searchBtn = document.getElementsByClassName('search-btn');
 //dùng loop để lấy tất cả các search-btn
 for (let i = 0; i < searchBtn.length; i++) {
-  searchBtn[i].onclick = function () {
-    searchDialog.style.transform = "translateY(-100%)"; // khởi tạo đưa về Y-100%
-    searchDialog.style.display = "grid"; // chuyển display sang block vì thẻ dialog default display none
-    let showSearchDialog = function () {
-      searchDialog.style.transform = "translateY(0%)"; // trả về Y 0%
+    searchBtn[i].onclick = function () {
+        searchDialog.style.transform = 'translateY(-100%)'; // khởi tạo đưa về Y-100%
+        searchDialog.style.display = 'grid'; // chuyển display sang block vì thẻ dialog default display none
+        let showSearchDialog = function () {
+            searchDialog.style.transform = 'translateY(0%)'; // trả về Y 0%
+        };
+        setTimeout(showSearchDialog, 0); // dùng bất đồng bộ để nhận transition
     };
-    setTimeout(showSearchDialog , 0); // dùng bất đồng bộ để nhận transition
-  }
 }
 
 //đóng search dialog bằng nút trong dialog
 
-let closeDialog = document.getElementById("dialog-closebtn");
+let closeDialog = document.getElementById('dialog-closebtn');
 
 closeDialog.onclick = function () {
-  if (window.innerWidth > 900) {
-    searchDialog.style.transform = "translateX(100%)"; // đưa về X 100%
-  } else {
-    searchDialog.style.transform = "translateY(-100%)"; // đưa về Y-100%
-  }
-  let resetTransform = function () { // reset lại display và translate
-    searchDialog.style.display = "none";
-    if (searchDialog.style.display == "none") {
-      searchDialog.style.transform = "translateX(0%)";
+    if (window.innerWidth > 900) {
+        searchDialog.style.transform = 'translateX(100%)'; // đưa về X 100%
+    } else {
+        searchDialog.style.transform = 'translateY(-100%)'; // đưa về Y-100%
     }
-  }
-  setTimeout(resetTransform , 500); // dùng bất đồng bộ để nhận transition
-}
+    let resetTransform = function () {
+        // reset lại display và translate
+        searchDialog.style.display = 'none';
+        if (searchDialog.style.display == 'none') {
+            searchDialog.style.transform = 'translateX(0%)';
+        }
+    };
+    setTimeout(resetTransform, 500); // dùng bất đồng bộ để nhận transition
+};
 
 // Video dialog
-let videoDialog = document.getElementById("video-dialog");
-let openVideoDialog = document.getElementById("open-video");
-let videoContent = document.getElementById("video-content");
+let videoDialog = document.getElementById('video-dialog');
+let openVideoDialog = document.getElementById('open-video');
+let videoContent = document.getElementById('video-content');
 
 if (openVideoDialog) {
-  openVideoDialog.onclick = function () {
-      videoDialog.style.display = "grid";
-      let showVideo = function () {
-        videoContent.style.transform = "scale(1)";
-      };
-      setTimeout(showVideo, 100);
-  } 
+    openVideoDialog.onclick = function () {
+        videoDialog.style.display = 'grid';
+        let showVideo = function () {
+            videoContent.style.transform = 'scale(1)';
+        };
+        setTimeout(showVideo, 100);
+    };
 }
 
-let closeVideoDialog = document.getElementById("video-close");
+let closeVideoDialog = document.getElementById('video-close');
 
 if (closeVideoDialog) {
-  closeVideoDialog.onclick = function () { // reset display
-    videoContent.style.transform = "scale(0)";
-    let resetDisplay = function () {
-      videoDialog.style.display = "none";
-    };
-    setTimeout(resetDisplay, 500);
+    closeVideoDialog.onclick = function () {
+        // reset display
+        videoContent.style.transform = 'scale(0)';
+        let resetDisplay = function () {
+            videoDialog.style.display = 'none';
+        };
+        setTimeout(resetDisplay, 500);
 
-    // reset video khi đóng video
-    var resetVideo = document.getElementById("video-content");      
-    //otherwise set scope as the entire document
-    //var myScope = document;
-    //if there is an iframe inside maybe embedded multimedia video/audio, we should reload so it stops playing
-    var iframes = resetVideo.getElementsByTagName("iframe");
-    if (iframes != null) {
-        for (var i = 0; i < iframes.length; i++) {
-            iframes[i].src = iframes[i].src; //causes a reload so it stops playing, music, video, etc.
+        // reset video khi đóng video
+        var resetVideo = document.getElementById('video-content');
+        //otherwise set scope as the entire document
+        //var myScope = document;
+        //if there is an iframe inside maybe embedded multimedia video/audio, we should reload so it stops playing
+        var iframes = resetVideo.getElementsByTagName('iframe');
+        if (iframes != null) {
+            for (var i = 0; i < iframes.length; i++) {
+                iframes[i].src = iframes[i].src; //causes a reload so it stops playing, music, video, etc.
+            }
         }
-    }
-  }
+    };
 }
 
 //Copllapsible content - updating
 
 // Click Slide next/prev button only
 
-let slides = document.querySelectorAll(".slide");
-
-// for (let i = 0; i < slides.length; i++) {
-//   slides.style.transform = `translateX(${i * 100}%)`;
-// }
-
-// dùng forEach thay thế vòng lặp for
+let slides = document.querySelectorAll('.slide');
 
 // loop through slides and set each slides translateX
 slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * 100}%)`;
-})
+    slide.style.zIndex = -indx;
+
+    slide.style.transform = `translateX(${indx * 100}%)`;
+});
 
 // current slide counter
 let curSlide = 0;
@@ -118,207 +115,205 @@ let curSlide = 0;
 let maxSlide = slides.length - 1;
 
 // select next slide button
-let nextSlide = document.querySelector(".next");
+let nextSlide = document.querySelector('.next');
 
 // add event listener and navigation functionality
 if (nextSlide) {
-  nextSlide.onclick = function () {
-  // check if current slide is the last and reset current slide
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
+    nextSlide.onclick = function () {
+        // check if current slide is the last and reset current slide
+        if (curSlide === maxSlide) {
+            curSlide = 0;
+        } else {
+            curSlide++;
+        }
 
-  // move slide by -100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-  };
-};
-
+        // move slide by -100%
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        });
+    };
+}
 
 // select next slide button
-let prevSlide = document.querySelector(".prev");
+let prevSlide = document.querySelector('.prev');
 
 // add event listener and navigation functionality
 if (prevSlide) {
-prevSlide.onclick = function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
+    prevSlide.onclick = function () {
+        // check if current slide is the first and reset current slide to last
+        if (curSlide === 0) {
+            curSlide = maxSlide;
+        } else {
+            curSlide--;
+        }
 
-  // move slide by 100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-};
-};
+        // move slide by 100%
+        slides.forEach((slide, indx) => {
+            slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        });
+    };
+}
 
 // Click Testimonial
-let qouteblock = document.querySelectorAll(".customer__content");
+let qouteblock = document.querySelectorAll('.customer__content');
 
 // loop through slides and set each slides translateX
 qouteblock.forEach((qoute, indx) => {
-  qoute.style.transform = `translateX(${indx * 100}%)`;
-})
+    qoute.style.transform = `translateX(${indx * 100}%)`;
+});
 
 // current slide counter
 let curQoute = 0;
 // maximum number of slides
 let maxQoute = qouteblock.length - 1;
 
-
-let nextQoute = document.querySelector(".ctm__dot--2");
+let nextQoute = document.querySelector('.ctm__dot--2');
 
 if (nextQoute) {
-nextQoute.onclick = function () {
-  // check if current slide is the last and reset current slide
-  if (curQoute === maxQoute) {
-    curQoute = 0;
-  } else {
-    curQoute++;
-  }
+    nextQoute.onclick = function () {
+        // check if current slide is the last and reset current slide
+        if (curQoute === maxQoute) {
+            curQoute = 0;
+        } else {
+            curQoute++;
+        }
 
-  // move slide by -100%
-  qouteblock.forEach((qoute, indx) => {
-    qoute.style.transform = `translateX(${100 * (indx - curQoute)}%)`;
-  });
-};
-};
+        // move slide by -100%
+        qouteblock.forEach((qoute, indx) => {
+            qoute.style.transform = `translateX(${100 * (indx - curQoute)}%)`;
+        });
+    };
+}
 
 // select next slide button
-let prevQoute = document.querySelector(".ctm__dot--1");
+let prevQoute = document.querySelector('.ctm__dot--1');
 
 if (prevQoute) {
-prevQoute.onclick = function () {
-  // check if current slide is the first and reset current slide to last
-  if (curQoute === 0) {
-    curQoute = maxQoute;
-  } else {
-    curQoute--;
-  }
+    prevQoute.onclick = function () {
+        // check if current slide is the first and reset current slide to last
+        if (curQoute === 0) {
+            curQoute = maxQoute;
+        } else {
+            curQoute--;
+        }
 
-  // move slide by 100%
-  qouteblock.forEach((qoute, indx) => {
-    qoute.style.transform = `translateX(${100 * (indx - curQoute)}%)`;
-  });
-};
-};
+        // move slide by 100%
+        qouteblock.forEach((qoute, indx) => {
+            qoute.style.transform = `translateX(${100 * (indx - curQoute)}%)`;
+        });
+    };
+}
 // Auto Slide - under development
 
 // reveal content animation
-let reveals = document.querySelectorAll(".reveal");
+let reveals = document.querySelectorAll('.reveal');
 
 function reveal() {
-  for (var i = 0; i < reveals.length; i++) {
-    let windowHeight = window.innerHeight;
-    // console.log(windowHeight);
-    let elementTop = reveals[i].getBoundingClientRect().top; // trả về khoảng cách từ top của window đến top của element
-    // console.log(elementTop);
-    let elementVisible = 150;
-    // console.log(windowHeight - elementVisible);
+    for (var i = 0; i < reveals.length; i++) {
+        let windowHeight = window.innerHeight;
+        // console.log(windowHeight);
+        let elementTop = reveals[i].getBoundingClientRect().top; // trả về khoảng cách từ top của window đến top của element
+        // console.log(elementTop);
+        let elementVisible = 150;
+        // console.log(windowHeight - elementVisible);
 
-    if (elementTop < (windowHeight - elementVisible)) {
-      // console.log("hello");
-      reveals[i].classList.add("showcontent");
+        if (elementTop < windowHeight - elementVisible) {
+            // console.log("hello");
+            reveals[i].classList.add('showcontent');
+        }
+        // else {
+        //   reveals[i].classList.remove("showcontent");
+        // }
     }
-    // else {
-    //   reveals[i].classList.remove("showcontent");
-    // }
-  }
 }
 
 if (reveals) {
-  window.addEventListener("scroll", reveal);
+    window.addEventListener('scroll', reveal);
 }
 
 // counting number animation
 
-let counterUp = document.querySelectorAll(".abt-sect2__counter");
+let counterUp = document.querySelectorAll('.abt-sect2__counter');
 
 function counterRun() {
-  for (var i = 0; i < counterUp.length; i++) {
-    let windowHeight = window.innerHeight;
-    let elementTop = counterUp[0].getBoundingClientRect().top;
-    let elementVisible = 10;
+    for (var i = 0; i < counterUp.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = counterUp[0].getBoundingClientRect().top;
+        let elementVisible = 10;
 
-    if (elementTop < windowHeight - elementVisible) {
-      counterUp[i].classList.add("counter");
-    } else {
-      counterUp[i].classList.remove("counter");
+        if (elementTop < windowHeight - elementVisible) {
+            counterUp[i].classList.add('counter');
+        } else {
+            counterUp[i].classList.remove('counter');
+        }
     }
-  }
 }
 
 if (counterUp) {
-  window.addEventListener("scroll", counterRun);
+    window.addEventListener('scroll', counterRun);
 }
 
 // ===JQ=== jquery có thể mix với js
-$(document).ready(function(){
+$(document).ready(function () {
+    //StickyNavbar
+    // $(window).scroll(function(){
+    //   if (window.pageYOffset > 0){
+    //     $("nav").addClass("stickyNav");
+    //   } else {
+    //     $("nav").removeClass("stickyNav");
+    //   }
+    // });
 
-  //StickyNavbar
-  // $(window).scroll(function(){
-  //   if (window.pageYOffset > 0){
-  //     $("nav").addClass("stickyNav");
-  //   } else {
-  //     $("nav").removeClass("stickyNav");
-  //   }
-  // });
+    // backtotop
+    // $(window).scroll(function(){
+    //   if (window.pageYOffset > 500){
+    //     $(".top").addClass("backTopBtn");
+    //   } else {
+    //     $(".top").removeClass("backTopBtn");
+    //   }
+    // });
 
-  // backtotop
-  // $(window).scroll(function(){
-  //   if (window.pageYOffset > 500){
-  //     $(".top").addClass("backTopBtn");
-  //   } else {
-  //     $(".top").removeClass("backTopBtn");
-  //   }
-  // });
+    // Dropdown Menu Main = JQ =
+    $('#dropbtn-main').click(function () {
+        $('.dropdown-content-main').slideToggle();
+    });
 
-  // Dropdown Menu Main = JQ =
-  $("#dropbtn-main").click(function(){
-    $(".dropdown-content-main").slideToggle();
-  });
+    // Dropdown Menu Hidden = JQ =
+    $('#dropbtn-hidden').click(function () {
+        $('#dropin').toggleClass();
+        $('#dropout').toggleClass('show');
+        $('.drop-content-hidden').slideToggle();
+    });
 
-  // Dropdown Menu Hidden = JQ =
-  $("#dropbtn-hidden").click(function(){
-    $("#dropin").toggleClass();
-    $("#dropout").toggleClass("show");
-    $(".drop-content-hidden").slideToggle();
-  });
+    //mở/đóng search dialog = JQ =
+    // $(".search-btn").click(function(){
+    //   $("#search-dialog").slideToggle();
+    // });
 
-  //mở/đóng search dialog = JQ =
-  // $(".search-btn").click(function(){
-  //   $("#search-dialog").slideToggle();
-  // });
+    //đóng search dialog bằng nút trong dialog = JQ =
+    // $("#dialog-closebtn").click(function(){
+    //   $("#search-dialog").slideUp();
+    // });
 
-  //đóng search dialog bằng nút trong dialog = JQ =
-  // $("#dialog-closebtn").click(function(){
-  //   $("#search-dialog").slideUp();
-  // });
+    //Dark/Light Btn
+    $('.dark-btn').click(function () {
+        $('.fa-moon').toggleClass('hide');
+        $('.fa-sun').toggleClass('show');
+        $(
+            'body, #navbar, .dropdown-content a, #search, #search-hidden, .search__div, .about-mid, #services, .ser-bot, .ser-layout2__items .ser-l1-items__content a, .ser-layout2__body--layout, .services-button, .article, .abt-l2-sub__content--sect1 .abt-l2-sub__text a, .abt-sect4__text a, .about__sect5, .services__sect2',
+        ).toggleClass('dark-theme');
+        if ($('#navbar').hasClass('dark-theme')) {
+            $('#navbar').removeClass('stickyNav');
+        } else if (window.pageYOffset > 0) {
+            $('#navbar').addClass('stickyNav');
+        }
+    });
 
-  //Dark/Light Btn
-  $(".dark-btn").click(function(){
-    $(".fa-moon").toggleClass("hide");
-    $(".fa-sun").toggleClass("show");
-    $("body, #navbar, .dropdown-content a, #search, #search-hidden, .search__div, .about-mid, #services, .ser-bot, .ser-layout2__items .ser-l1-items__content a, .ser-layout2__body--layout, .services-button, .article, .abt-l2-sub__content--sect1 .abt-l2-sub__text a, .abt-sect4__text a, .about__sect5, .services__sect2").toggleClass("dark-theme");
-    if ($("#navbar").hasClass("dark-theme")){
-      $("#navbar").removeClass("stickyNav");
-    } else if (window.pageYOffset > 0) {
-      $("#navbar").addClass("stickyNav");
-    }
-  });
-
-  //Copllapsible content
-  $(".copllapse__btn").click(function(){
-    $(this).toggleClass("copllapse__btn--gb");
-    $(this).children(".copllapse__down").toggleClass("hide");
-    $(this).children(".copllapse__up").toggleClass("show");
-    $(this).next().slideToggle();
-  });
+    //Copllapsible content
+    $('.copllapse__btn').click(function () {
+        $(this).toggleClass('copllapse__btn--gb');
+        $(this).children('.copllapse__down').toggleClass('hide');
+        $(this).children('.copllapse__up').toggleClass('show');
+        $(this).next().slideToggle();
+    });
 });
-
